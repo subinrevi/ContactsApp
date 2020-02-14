89f192203ef88b87dd8a9d0dd80bcd361f9e6fe3
subin.revi <subin.revi@gmail.com>
2020-02-14 10:47:53 +0530

    First commit to Contacts App

---
+++ ContactsApp/ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by subin.revi on 14/02/20.
+//  Copyright © 2020 subin.revi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
