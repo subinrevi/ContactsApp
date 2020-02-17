bd39a69a9238914485f15563a289541d85c6daa1
subin.revi <subin.revi@gmail.com>
2020-02-17 13:48:04 +0530

    Created Header view for Add Contact screen

--- ContactsApp/ContactsApp/ContactsModule/View/AddContactViewController.swift
+++ ContactsApp/ContactsApp/ContactsModule/View/AddContactViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class AddContactViewController: UIViewController {
 
+    @IBOutlet weak var addContactTableView: UITableView!
+    let headerView = HeaderView.instantiate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHeaderView()
+       
     }
+    
+    func setupHeaderView() {
+        addContactTableView.estimatedRowHeight = 50
+        addContactTableView.contentInset = UIEdgeInsets(top: 300, left: 0, bottom: 0, right: 0)
+        addContactTableView.backgroundColor = .lightGray
+    
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(headerView)
+        headerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        headerView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        headerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        headerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 0).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+    }
+    
 
     //MARK: Button Actions
     @IBAction func doneButtonTapped(_ sender: Any) {
@@ -23,4 +43,18 @@ class AddContactViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+}
+
+
+extension AddContactViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Test \(indexPath.row)"
+        return cell
+    }
 }
---
+++ ContactsApp/ContactsApp/CustomViews/HeaderView.swift
@@ -0,0 +1,34 @@
+//
+//  HeaderView.swift
+//  ContactsApp
+//
+//  Created by subin.revi on 17/02/20.
+//  Copyright © 2020 subin.revi. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //instantiate()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        //instantiate()
+    }
+
+    static func instantiate() -> HeaderView {
+        let view: HeaderView = .initFromNib()
+        return view
+    }
+}
+
+
+extension UIView {
+    class func initFromNib<T: UIView>() -> T {
+        return Bundle.main.loadNibNamed(String(describing: self), owner: nil, options: nil)?[0] as! T
+    }
+}
